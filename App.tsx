8f07d2a25516f1ffe1ebfb2f90dd962628688fd9
santiagomanso <santiagomanso@web.de>
2023-11-06 19:31:31 +0100

    🎉 initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Test from './src/components/test';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  return (
+    <>
+      <Text>App Screen</Text>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Test = () => {
+  return <Text>QUE ONDAAA</Text>;
+};
+
+export default Test;
